454ca7b9e8ca957ffa9ae63958ba9213ca17a715
Clément Nerma <clement.nerma@gmail.com>
2022-10-17 00:25:54 +0200

    Add query to check docker compose version

--- src/docker/detect.rs
+++ src/docker/detect.rs
@@ -8,3 +8,7 @@ static DOCKER_COMPOSE_CMD: &str = "docker compose";
 pub fn docker_version() -> Result<String> {
     run_cmd(DOCKER_CMD, &["version"])
 }
+
+pub fn docker_compose_version() -> Result<String> {
+    run_cmd(DOCKER_COMPOSE_CMD, &["version"])
+}
--- src/server/queries.rs
+++ src/server/queries.rs
@@ -16,4 +16,8 @@ impl QueryRoot {
     async fn docker_version(&self) -> Result<String> {
         docker::docker_version().map_err(format_err)
     }
+
+    async fn docker_compose_version(&self) -> Result<String> {
+        docker::docker_compose_version().map_err(format_err)
+    }
 }
