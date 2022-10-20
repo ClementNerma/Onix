6d688edcfadb485bed11163ff341afa2dd0019a5
Clément Nerma <clement.nerma@gmail.com>
2022-10-20 19:20:16 +0200

    Remove unneeded Docker module

--- src/docker/images.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::{Context, Result};
-use bollard::{service::ImageSummary, Docker};
-
-pub async fn list_images(docker: &Docker) -> Result<Vec<ImageSummary>> {
-    docker
-        .list_images::<String>(None)
-        .await
-        .context("Failed to list existing Docker images")
-}
--- src/docker/mod.rs
+++ src/docker/mod.rs
@@ -1,9 +1,7 @@
 mod about;
 mod constants;
 mod containers;
-mod images;
 
 pub use about::*;
 pub use constants::*;
 pub use containers::*;
-pub use images::*;
