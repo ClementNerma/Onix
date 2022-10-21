0bb15350a6e59b773ecc8cd002f85cda15055cd5
Clément Nerma <clement.nerma@gmail.com>
2022-10-21 15:10:32 +0200

    Add basic template

--- onix-client/src/App.svelte
+++ onix-client/src/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import { Router } from 'svelte-navigator'
   import AppRouter from './AppRouter.svelte'
+  import Template from './templates/Template.svelte'
 </script>
 
 <Router primary={false}>
-  <AppRouter />
+  <Template>
+    <AppRouter />
+  </Template>
 </Router>
---
+++ onix-client/src/templates/Template.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import SideNav from '../organisms/SideNav.svelte'
+</script>
+
+<div class="template">
+  <div class="side-nav">
+    <SideNav />
+  </div>
+
+  <div class="template-slot">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .template {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: row;
+  }
+</style>
