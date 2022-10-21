c30014018a1fe054ccefb8ec1e30584586b78fe0
Clément Nerma <clement.nerma@gmail.com>
2022-10-21 15:13:00 +0200

    Change client's default port

--- onix-client/vite.config.ts
+++ onix-client/vite.config.ts
@@ -5,7 +5,7 @@ import suidPlugin from '@suid/vite-plugin'
 export default defineConfig({
   plugins: [solidPlugin(), suidPlugin()],
   server: {
-    port: 3000,
+    port: 5170,
   },
   build: {
     target: 'esnext',
