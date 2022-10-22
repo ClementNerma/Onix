d9f11461e31ae6121b6890b0115f1f76506ff7d1
Clément Nerma <clement.nerma@gmail.com>
2022-10-22 14:33:33 +0200

    Remake routing

--- onix-client/src/App.tsx
+++ onix-client/src/App.tsx
@@ -1,23 +1,30 @@
 import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import { client } from './graphql/client'
 import { HomePage } from './pages/HomePage'
 import { validateRoute } from './routing'
 import { Template } from './templates/Template'
 
 export const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: validateRoute('/'),
+      element: <Template />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+      ],
+    },
+  ])
+
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter>
-        <ChakraProvider>
-          <Routes>
-            <Route path={validateRoute('/')} element={<Template />}>
-              <Route index element={<HomePage />} />
-            </Route>
-          </Routes>
-        </ChakraProvider>
-      </BrowserRouter>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
     </ApolloProvider>
   )
 }
