84295daabb0cabf2fb4206753be96859d1185d13
Clément Nerma <clement.nerma@gmail.com>
2022-10-24 16:04:06 +0200

    Unify app action buttons

--- onix-client/src/molecules/AppActions/index.tsx
+++ onix-client/src/molecules/AppActions/index.tsx
@@ -1,10 +1,13 @@
 import { ButtonProps, Tag } from '@chakra-ui/react'
-import { AppRunningStatus } from '../../graphql/generated'
+import {
+  AppRunningStatus,
+  useCreateAppContainerMutation,
+  useStartAppMutation,
+  useStopAppMutation,
+} from '../../graphql/generated'
 import { assertNever } from '../../utils'
-import { CreateAppContainersButton } from '../../atoms/CreateAppContainersButton'
-import { StopAppButton } from '../../atoms/StopAppButton'
-import { StartAppButton } from '../../atoms/StartAppButton'
-import { ActionButtonState } from '../../atoms/ActionButton'
+import { ActionButton, ActionButtonState } from '../../atoms/ActionButton'
+import { MdAddCircle, MdPlayArrow, MdStop } from 'react-icons/md'
 
 export type AppActionProps = {
   appId: string
@@ -34,4 +37,67 @@ export const AppActions = ({ appId, status, onStateChange, onFinished, ...rest }
     default:
       return assertNever(status)
   }
+}
+
+type AppActionButtonProps = {
+  appId: string
+  onStateChange?: (state: ActionButtonState) => void
+  onFinished?: (succeeded: boolean) => void
+} & Omit<ButtonProps, 'onClick'>
+
+const CreateAppContainersButton = ({ appId, onStateChange, onFinished, ...rest }: AppActionButtonProps) => {
+  const [createAppContainers, result] = useCreateAppContainerMutation()
+
+  return (
+    <ActionButton
+      icon={<MdAddCircle />}
+      colorScheme="blue"
+      size="sm"
+      onClick={() => createAppContainers({ variables: { id: appId } })}
+      label="Create app containers"
+      state={result}
+      errorTitle="Failed to create application"
+      onStateChange={onStateChange}
+      onFinished={onFinished}
+      {...rest}
+    />
+  )
+}
+
+const StartAppButton = ({ appId, onStateChange, onFinished, ...rest }: AppActionButtonProps) => {
+  const [startApp, result] = useStartAppMutation()
+
+  return (
+    <ActionButton
+      icon={<MdPlayArrow />}
+      colorScheme="blue"
+      size="sm"
+      onClick={() => startApp({ variables: { id: appId } })}
+      label="Start"
+      state={result}
+      errorTitle="Failed to start the application"
+      onStateChange={onStateChange}
+      onFinished={onFinished}
+      {...rest}
+    />
+  )
+}
+
+const StopAppButton = ({ appId, onStateChange, onFinished, ...rest }: AppActionButtonProps) => {
+  const [stopApp, result] = useStopAppMutation()
+
+  return (
+    <ActionButton
+      icon={<MdStop />}
+      colorScheme="yellow"
+      size="sm"
+      onClick={() => stopApp({ variables: { id: appId } })}
+      label="Stop"
+      state={result}
+      errorTitle="Failed to stop the application"
+      onStateChange={onStateChange}
+      onFinished={onFinished}
+      {...rest}
+    />
+  )
 }
