fa125d76aeefff1455210580a9c246b2f507a10b
Clément Nerma <clement.nerma@gmail.com>
2022-10-24 16:30:42 +0200

    Disable cache for queries

--- onix-client/src/graphql/client.ts
+++ onix-client/src/graphql/client.ts
@@ -3,4 +3,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const client = new ApolloClient({
   uri: 'http://localhost:5871',
   cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'no-cache',
+    },
+  },
 })
